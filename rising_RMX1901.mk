bdf97bd89257248299a6197bf94e92ae1e1a5041
Shravankumar <shravankumarsayz@gmail.com>
2024-02-22 14:36:35 +0530

    RMX1901: rising_RMX1901.mk

--- rising_RMX1901.mk
+++ rising_RMX1901.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit some common Rising OS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
