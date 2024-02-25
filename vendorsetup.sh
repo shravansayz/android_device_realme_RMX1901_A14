4abd1be78f65f139fdf9aa8b04e2b2fb8d65c432
Shravankumar <shravankumarsayz@gmail.com>
2024-02-25 12:11:44 +0530

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-echo 'Sub module [1/1]'
-
-cd kernel/realme/sdm710 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
