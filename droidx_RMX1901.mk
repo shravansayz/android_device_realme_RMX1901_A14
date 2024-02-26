4c009860b86c84a1ca299b02be0e4ea51b6349ec
Shravankumar <shravankumarsayz@gmail.com>
2024-02-26 13:13:55 +0530

    RMX1901: Blunder

--- droidx_RMX1901.mk
+++ droidx_RMX1901.mk
@@ -27,7 +27,7 @@ RISING_CHIPSET := SDM710
 RISING_BATTERY := 3765mAh
 
 # Inherit from RMX1901 device
-$(call inherit-product, $(LOCAL_PATH)/device.mk
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 
 PRODUCT_BRAND := realme
