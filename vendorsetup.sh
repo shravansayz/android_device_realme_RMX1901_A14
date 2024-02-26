9f5663fe3f56f522c594309b076cbdcc67b04bc5
Shravankumar <shravankumarsayz@gmail.com>
2024-02-26 10:03:24 +0530

    RMX1901: Adding vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#kernel tree
+https://github.com/shravansayz/android_kernel_realme_sdm710.git -b 14 kernel/realme/sdm710
+
+#Vendor tree
+https://github.com/shravansayz/android_vendor_RMX1901_A14.git -b 14 vendor/realme/RMX1901
+
+#Clang-proton
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git  prebuilts/clang/host/linux-x86/clang-proton
