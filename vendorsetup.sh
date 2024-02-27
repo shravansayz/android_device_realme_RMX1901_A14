dcdf105189b8332f7b69e93b6027388c453c455a
Shravankumar <shravankumarsayz@gmail.com>
2024-02-27 15:25:19 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,2 @@
-echo 'Cloning Kernel tree [1/3]'
-https://github.com/shravansayz/android_kernel_realme_sdm710.git -b 14 realme/sdm710
-
-echo 'Cloning Vendor tree [2/3]'
-https://github.com/shravansayz/android_vendor_RMX1901_A14.git -b 14 realme/RMX1901
-
-echo 'cloning Clang-proton [3/3]'
+echo 'cloning Clang-proton [1/1]'
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git  prebuilts/clang/host/linux-x86/clang-proton
