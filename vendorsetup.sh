b43c78d4fcf9e8f8aea73a92088ec9fbe60a0528
Shravankumar <shravankumarsayz@gmail.com>
2024-03-04 14:15:23 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
-CLANGDIR=$PWD/prebuilts/clang/host/linux-x86/clang-proton
-CLANGVER=proton
+CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
+CLANGVER=clang-r498229b
 if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading proton${NC}"
-    git clone --depth=1 https://github.com/shravansayz/clang-proton.git -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
+    echo -e "${GREEN}Downloading clang 17.0.4${NC}"
+    git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
