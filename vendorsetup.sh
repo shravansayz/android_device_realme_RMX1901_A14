07eb72c6ed56b319d94261633b18ebf9f0d61c2a
Shravankumar <shravankumarsayz@gmail.com>
2024-03-04 13:53:53 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,5 @@ then
     GREEN='\033[0;32m'
     NC='\033[0m'
     echo -e "${GREEN}Downloading proton${NC}"
-    git clone --depth=1 https://github.com/kdrag0n/proton-clang.git  prebuilts/clang/host/linux-x86/clang-proton -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
+    git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
