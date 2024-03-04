64ca9ae3254b798f54f8eb75fce0a94ea08b2e46
Shravankumar <shravankumarsayz@gmail.com>
2024-03-04 13:00:30 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 echo "Syncing KernelSU to latest source [1/1]"
-cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
+cd kernel/realme/sdm710 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
